38bbb6a2ef0b2817e96cc4e90f4c683ee40452ee
Gustavo <gglagorio@gmail.com>
2023-07-02 03:05:31 -0300

    Agregado de iconos

---
+++ javascript/articulos.js
@@ -0,0 +1,16 @@
+let tortas = {
+    chocotorta: {
+        imagen: "../assets/torta9.jpeg",
+        precio: 2500,
+        porciones: "<ul><li>6</li><li>10</li><li>14</li></ul>",
+        ingredientes: "<ul><li>Ingrediente 1</li><li>Ingrediente 2</li><li>Ingrediente 3</li><li>Ingrediente 4</li></ul>",
+        categoria: "postres"
+    },
+    redVelvet: {
+        imagen: "../assets/cupcake1.jpeg",
+        precio: 3500,
+        porciones: "<ul><li>6</li><li>10</li><li>14</li></ul>",
+        ingredientes: "<ul><li>Ingrediente 1</li><li>Ingrediente 2</li><li>Ingrediente 3</li><li>Ingrediente 4</li></ul>",
+        categoria: "postres"
+    }
+}
